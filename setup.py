172f2bd9b0235b2b3e2b5c15fae8336bc88dc16c
broamski <brian.nuszkowski@me.com>
2015-02-08 13:00:15 -0500

    Add minimum boto version

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='botomfa',
     version='0.2',
     author='Brian Nuszkowski',
     py_modules=['botomfa'],
-    install_requires=['boto'],
+    install_requires=['boto>=2.35.2'],
     data_files=[('/usr/bin', ['botomfa'])]
 )
